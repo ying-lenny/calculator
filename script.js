21203e5bd967299b47d1cc4cdc182686b9fe56fc
Owen Lenihan <owenlenihan101@gmail.com>
2023-05-09 12:53:45 +0100

    Added more buttons and functionality
    -Added buttons for each number that can be added to the current operation.
    -Renamed operator buttons to their symbols and a switch function to act on the selected operator.
    -Added Delete and clear buttons with working functionality.
    -Added a calculator favicon.

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
+let currentOp = null;
 
-
-
-const operatorButtons = document.querySelectorAll('[operator-button]')
-const deleteButton = document.getElementById('delete-button')
+const currentOperation = document.getElementById('current-op-screen');
+const operatorButtons = document.querySelectorAll('[operator-button]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const deleteButton = document.getElementById('delete-button');
+const resetButton = document.getElementById('reset-button')
 
 // Trigger function on selection of operator buttons
 operatorButtons.forEach((button) => 
 	button.addEventListener('click', () => test(button.textContent))
 )
 
+numberButtons.forEach((button) => 
+	button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
 // Triggers Delete function
-deleteButton.addEventListener('click', deleteNumber)
+deleteButton.addEventListener('click', deleteNumber);
+// Triggers Reset function
+resetButton.addEventListener('click', resetScreen);
 
 function test(operator) {
-	console.log(`Hi, you selected ${operator}`)
+	calculate(operator)
 }
 
 function deleteNumber() {
-	console.log("Hello")
+	currentOperation.textContent = currentOperation.textContent.toString().slice(0, -1);
+}
+
+function resetScreen() {
+	currentOperation.textContent = 0
+}
+
+function appendNumber(number) {
+	currentOperation.textContent += number;
+}
+
+function calculate(operator) {
+	switch(operator) {
+		case '+':
+			console.log(`Add: ${operator}`)
+			return;
+		case '-':
+			console.log(`Subtract: ${operator}`)
+			return;
+		case 'X':
+			console.log(`Multiply: ${operator}`)
+			return;
+		case '÷':
+			console.log(`Divide: ${operator}`)
+			return;
+	}
 }
